e1b7d38cf8de034481f59629e94fe92c9b8a280a
Ivan Bogdanov <c3t3o7@gmail.com>
2020-07-31 17:20:05 +0400

    Fix conversion grayscale cv::Mat to QPixmap

--- utils.cpp
+++ utils.cpp
@@ -1,13 +1,25 @@
 #include "utils.h"
 
+#include <QImage>
+
 namespace utils {
 QPixmap cvMat2QPixmap(const cv::Mat& mat)
 {
+    QImage::Format format;
+    switch(mat.channels())
+    {
+    case 1:
+        format = QImage::Format_Grayscale8;
+        break;
+    default:
+        format = QImage::Format_BGR888;
+        break;
+    }
     return QPixmap::fromImage(QImage{
         reinterpret_cast<const uchar*>(mat.data),
         mat.cols,
         mat.rows,
         static_cast<int>(mat.step),
-        QImage::Format_BGR888});
+        format});
 }
 } // namespace utils
