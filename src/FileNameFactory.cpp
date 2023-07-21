64fbf192b4965fd1ed54cecafd1ead74d86af9ee
vehlwn <36479428+vehlwn@users.noreply.github.com>
2023-07-21 18:19:41 +0400

    Use Boost.Date_Time to format time

---
+++ src/FileNameFactory.cpp
@@ -0,0 +1,45 @@
+#include "FileNameFactory.hpp"
+
+#include <filesystem>
+#include <sstream>
+
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+namespace vehlwn {
+namespace {
+std::string
+    format_ptime(const boost::posix_time::ptime& ptime, const char* const format)
+{
+    auto facet = new boost::posix_time::time_facet(format);
+    auto os = std::ostringstream();
+    os.imbue(std::locale(os.getloc(), facet));
+    os << ptime;
+    return os.str();
+}
+
+std::string get_date_component(const boost::posix_time::ptime& ptime)
+{
+    return format_ptime(ptime, "%Y-%m-%d");
+}
+
+std::string get_time_component(const boost::posix_time::ptime& ptime)
+{
+    return format_ptime(ptime, "%H:%M:%s");
+}
+
+} // namespace
+
+std::string DateFolderFactory::generate()
+{
+    const auto now = boost::posix_time::microsec_clock::local_time();
+    const auto date_component = get_date_component(now);
+    const auto time_component = get_time_component(now);
+
+    auto ret = std::filesystem::path(m_prefix);
+    ret /= date_component;
+    std::filesystem::create_directories(ret);
+    ret /= time_component;
+    ret += m_extension;
+    return ret.string();
+}
+} // namespace vehlwn
--- src/FileNameFactory.hpp
+++ src/FileNameFactory.hpp
@@ -1,11 +1,6 @@
 #pragma once
 
-#include <filesystem>
-#include <sstream>
-
-#include <boost/chrono/io/time_point_io.hpp>
-#include <boost/chrono/io/timezone.hpp>
-#include <boost/chrono/system_clocks.hpp>
+#include <string>
 
 namespace vehlwn {
 class FileNameFactory {
@@ -35,24 +30,7 @@ public:
     {
         m_extension = std::move(s);
     }
-    std::string generate() override
-    {
-        const auto now = boost::chrono::system_clock::now();
-        std::ostringstream os;
-        using boost::chrono::time_fmt, boost::chrono::timezone;
-        os << time_fmt(timezone::local, "%Y-%m-%d") << now;
-        const auto date_component = os.str();
-        os.str("");
-        os << time_fmt(timezone::local, "%H.%M.%S") << now;
-        const auto time_component = os.str();
-
-        std::filesystem::path ret = m_prefix;
-        ret /= date_component;
-        std::filesystem::create_directories(ret);
-        ret /= time_component;
-        ret += m_extension;
-        return ret.string();
-    }
+    std::string generate() override;
 };
 
 } // namespace vehlwn
